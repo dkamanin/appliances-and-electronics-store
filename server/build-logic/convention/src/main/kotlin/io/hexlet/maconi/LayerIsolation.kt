3fca15e79f04c50e5014450927cf37fafb598f66
Daniil Kamanin <me@kamanin.dev>
2026-04-08 17:40:21 +0300

    build(build-logic): configure license header check in Spotless
    Updated Spotless to automatically manage license headers in .java, .kt, and .gradle.kts files project-wide. Added ktlint for Gradle scripts and configured .editorconfig with Kotlin formatting rules.

--- server/build-logic/convention/src/main/kotlin/io/hexlet/maconi/LayerIsolation.kt
+++ server/build-logic/convention/src/main/kotlin/io/hexlet/maconi/LayerIsolation.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2026 Daniil Kamanin and contributors.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ */
+
 package io.hexlet.maconi
 
 import org.gradle.api.InvalidUserDataException
--- server/build-logic/convention/src/main/kotlin/io/hexlet/maconi/Spotless.kt
+++ server/build-logic/convention/src/main/kotlin/io/hexlet/maconi/Spotless.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2026 Daniil Kamanin and contributors.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ */
+
 package io.hexlet.maconi
 
 import com.diffplug.gradle.spotless.SpotlessExtension
@@ -7,8 +17,15 @@ import org.gradle.kotlin.dsl.configure
 internal fun Project.configureSpotlessForJvm() {
     extensions.configure<SpotlessExtension> {
         java {
+            target("src/**/*.java")
+            licenseHeaderFile(rootDir.resolve("spotless/copyright.java"))
             googleJavaFormat(libs.findVersion("googleJavaFormat").get().requiredVersion).aosp()
-            target("**/*.java")
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("*.kts")
+            licenseHeaderFile(rootDir.resolve("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+            ktlint()
             endWithNewline()
         }
     }
@@ -18,6 +35,13 @@ internal fun Project.configureSpotlessForRoot() {
     extensions.configure<SpotlessExtension> {
         kotlin {
             target("build-logic/convention/src/**/*.kt")
+            licenseHeaderFile(rootDir.resolve("spotless/copyright.kt"))
+            ktlint()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("*.kts", "build-logic/*.kts", "build-logic/convention/*.kts")
+            licenseHeaderFile(rootDir.resolve("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
             ktlint()
             endWithNewline()
         }
