a0f6f46a1d11d4e01ca7c8b5f72ae8019a22adbe
Daniil Kamanin <me@kamanin.dev>
2026-04-08 17:40:21 +0300

    build(build-logic): configure license header check in Spotless
    Updated Spotless to automatically manage license headers in .java, .kt, and .gradle.kts files project-wide. Added ktlint for Gradle scripts and configured .editorconfig with Kotlin formatting rules.

---
+++ server/spotless/copyright.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) $YEAR Daniil Kamanin and contributors.
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/MIT
+ */
+
